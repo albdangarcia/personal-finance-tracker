5811a5b80e6cc91646a4e256d34e6621eac25364
Daniel Garcia <albdangar@gmail.com>
2026-05-21 20:24:54 -0400

    feat: install tsx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,14 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+      }],
+    },
+  },
 ]);
 
 export default eslintConfig;
